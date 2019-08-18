ab0de4f8b60484c1473fe379f59fe7296a09a209
jpromanonet <programming@jpromano.net>
2019-08-17 21:24:58 -0300

    adding JS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-window.
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+
+  // Sounds playing!
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].play();
+    });
+  });
+});
