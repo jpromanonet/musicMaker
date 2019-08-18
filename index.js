231760e40769b57af20dd32add9d1a099fcb539d
jpromanonet <programming@jpromano.net>
2019-08-17 21:37:46 -0300

    Visual effects added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#6860d3",
+    "#60b2d3"
+  ];
 
   // Sounds playing!
   pads.forEach((pad, index) => {
