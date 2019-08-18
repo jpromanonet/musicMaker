d332c7636d7112a2c06911d2fa0dd5f7a9461c1e
jpromanonet <programming@jpromano.net>
2019-08-17 21:48:37 -0300

    Finished!!!

--- index.js
+++ index.js
@@ -26,5 +26,8 @@ window.addEventListener("load", () => {
     visual.appendChild(bubble);
     bubble.style.backgroundColor = colors[index];
     bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
   };
 });
